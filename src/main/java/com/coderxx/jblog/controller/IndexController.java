955440f71d468aa053ac95f594fa455a6e04131a
zhangjian <184263567@qq.com>
2018-12-01 11:50:30 +0800

    thymeleaf

--- src/main/java/com/coderxx/jblog/controller/IndexController.java
+++ src/main/java/com/coderxx/jblog/controller/IndexController.java
@@ -1,6 +1,7 @@
 package com.coderxx.jblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,9 @@ public class IndexController {
 
     @RequestMapping("/")
 //    @GetMapping("/hello/{id}")
-    public String index() {
+    public String index(Model model) {
+        String name = "jiangbei";
+        model.addAttribute("name", name);
 //        return "Hello World!";
         return "/index";
     }
