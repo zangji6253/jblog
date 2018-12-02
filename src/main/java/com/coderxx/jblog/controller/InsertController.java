f80e9ac1e74d1913ab9674a4184b1f97d8be80b1
zhangjian <184263567@qq.com>
2018-12-02 15:51:23 +0800

    insert done

--- src/main/java/com/coderxx/jblog/controller/InsertController.java
+++ src/main/java/com/coderxx/jblog/controller/InsertController.java
@@ -1,12 +1,16 @@
 package com.coderxx.jblog.controller;
 
+import com.coderxx.jblog.entity.Article;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class InsertController {
     @RequestMapping("/insert")
-    public String index() {
+    public String index(Model model) {
+        Article article = new Article();
+        model.addAttribute("article", article);
         return "/insert";
     }
 }
