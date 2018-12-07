a2043ef7582bb1fd7571ed2dc86328051efb6d7c
Jian Zhang <184263567@qq.com>
2018-12-07 14:09:17 +0800

    scp

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 mvn clean install -DskipTests
 scp target/jblog-0.0.1-SNAPSHOT.jar root@www.coderxx.com:/root/public
-java -jar target/jblog-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod
+ps -aux | less
+java -Xms128m -Xmx2048m -jar ./jblog-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod 5 >./logs/"$now"_bidcheck.log &
